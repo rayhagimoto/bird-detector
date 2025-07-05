6425ecb1e9724c09dadee22df315012230b75828
Ray Hagimoto <ray.m.hagimoto@gmail.com>
2025-07-05 17:28:13 -0500

    simple branch: prune to minimal files for SimpleDetector Lambda deployment

--- autoencoder/__init__.py
+++
@@ -1,5 +0,0 @@
-from .torch_utils import get_loss_function, get_optimizer
-
-from .fully_connected import SimpleAutoencoderDetector, SimpleAutoencoder
-from .convolution import ConvAutoencoder
-from .train import train_model
